2f68a8c77af87b3e8248b598b0d111534ca66c93
Ferdi Sefa Düzgün <76045185+ferdisefaduzgun@users.noreply.github.com>
2023-05-20 14:29:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 document.getElementById('mode-btn').addEventListener("click", () => {
-    document.body.classList.toggle("darkmodee");
-})
-
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('a').classList.toggle("darkmodeee");
-})
-
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('b').classList.toggle("darkmodeee");
-})
-
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('c').classList.toggle("darkmodeee");
-})
-
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('d').classList.toggle("darkmodeee");
-})
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('e').classList.toggle("darkmodeee");
-})
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('f').classList.toggle("darkmodeee");
-})
-document.getElementById('mode-btn').addEventListener("click", () => {
-    document.getElementById('g').classList.toggle("darkmodeee");
+    document.body.classList.toggle("darkmode");
 })
 
 
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('b').classList.toggle("darkmodee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('z').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('l').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('x').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('k').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('j').classList.toggle("darkmodeee");
+})
+
+document.getElementById('mode-btn').addEventListener("click", () => {
+    document.getElementById('u').classList.toggle("darkmodeeee");
+})
