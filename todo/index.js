cfe1f178d60becb84b0ad45f6f9b745a7a610855
Ferdi Sefa Düzgün <76045185+ferdisefaduzgun@users.noreply.github.com>
2023-06-16 17:02:06 +0300

    Add files via upload

--- todo/index.js
+++ todo/index.js
@@ -1,99 +1,186 @@
-//1. olay inputun içersinde yazılan veriyi alıyoruz.
-const inputDom = document.getElementById('yaz');
-let textInputValue = ""; // inputa yazılan valueler için bir let oluşturduk
-inputDom.addEventListener("change", function(event){
-    textInputValue = event.target.value // oluşturduğumuz leti içersindeki yazıyı çekmeye değiştik.
-}) //olay ekledik ve olayın tipini belirttik.(change)
-
-//2. olay ekle kısmındaki tıklamayı almak
-let eklenecekler = []; //bir array oluşturduk.
-const ekleButton = document.getElementById("ekle");
-ekleButton.addEventListener("click", tiklama)
-function tiklama(e){
-    e.preventDefault(); //sayfa yenilenmesi durdurma (bir yere tıklanınca)
-    eklenecekler.push({id: eklenecekler.length + 1, baslik: textInputValue});
-    //inputDom = document.getElementById('yaz').value = ""; // inputu seçtik ve içine bişey girip tıklandıktan sonra silinmesini söyledik.
-    inputDom.value = ""; // yukarıdakinin kısası
-    displayTodos(); // tıklandığında displayi çalıştır.
-}
-//3 ul listeye ulaşıp yazdıklarımızı ekletme
-const ulListe = document.querySelector("#ul-ekle");
-
-let date = new Date()
-let year = date.getFullYear(),
-    month = date.getMonth(),
-    day = date.getDate(),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    second = date.getSeconds(),
-    dayNumber = date.getDay()
-
-    let months = [
-        'Ocak',
-        'Şubat',
-        'Mart',
-        'Nisan',
-        'Mayıs',
-        'Haziran',
-        'Temmuz',
-        'Ağustos',
-        'Eylül',
-        'Ekim',
-        'Kasım',
-        'Aralık',
-    ]
-
-    let days = [
-        'Pazar',
-        'Pazartesi',
-        'Salı',
-        'Çarşamba',
-        'Perşembe',
-        'Cuma',
-        'Cumartesi'
-    ]
+/* selectors */
+const todoInput = document.querySelector('.todoInput');
+const addButton = document.querySelector('.addButton');
+const todoList = document.querySelector('.todoList');
+const todoFilter = document.querySelector(".filter-todo");
 
 
+/* alerts */
+const success = document.querySelector('.success');
+const warning = document.querySelector('.warning');
 
-function displayTodos(){  // yeni fonk açtık
-    if(eklenecekler.length === 0){  // liste boşsa ul için eliste boş yazdırdık
-        ulListe.innerHTML = "liste boş!"
+/* events */
+document.addEventListener("DOMContentLoaded", function () {
+    getTodos();
+});
+addButton.addEventListener("click", addTodo);
+todoList.addEventListener('click', checks)
+todoFilter.addEventListener("click", filterTodo)
+
+/* functions */
+
+
+//ekleme fonksiyonu ve alarmlar
+function addTodo(e){
+
+    e.preventDefault();
+
+    let todoValue = todoInput.value; 
+
+/*  const isEmpty = str => !str.trim().length;
+
+    if (isEmpty(todoInput.value)) 
+    boyle de string boş mu  değil mi kontrol edilir.
+
+*/ 
+
+    if(todoValue == 0){
+        warning.style.display = 'block';
+        setTimeout(() => {
+            warning.style.display = 'none';
+        }, 1500);
     }else{
-        let result = "";  // boş string açtık 
-        eklenecekler.forEach((item) => { //ekleneceklerin içinde kaç değer varsa dön (foreach). // item dönen elemanları temsil eder. //her eklenince şunu yap (result)       
-        result += `                  
-            <li>
-            <span id="ul-ekle">${item.baslik}</span>
-            <button onclick="deleteListe(${item.id})" >Sil</button>
-            </li>
-            ${day} ${months[month]} ${days[dayNumber]}
-            `
-        }
-    
-        )
-        ulListe.innerHTML = result; // her eklediğinde listenin içersine at
-    }
-    }
 
-function deleteListe(id){
-    let deletedId;
-    
-    for(let index in eklenecekler){
-        if(eklenecekler[index].id == id){
-            deletedId = index;
-        }
+        saveLocalTodos(todoInput.value);
 
-        eklenecekler.splice(deletedId, 1)
-        displayTodos();
+        e.preventDefault();
+
+        const addDıv = document.createElement("div");
+        addDıv.classList.add('todo');
+        
+        const addButton1 = document.createElement('button');
+        addButton1.classList.add('checkedButton');
+        addButton1.innerHTML = '<i class="fa-solid fa-check"></i>';
+        addDıv.appendChild(addButton1);
+    
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('todo-item');
+        todoItem.innerText = todoInput.value;
+        addDıv.appendChild(todoItem);
+    
+        const addButton2 = document.createElement('button');
+        addButton2.classList.add('deleteButton');
+        addButton2.innerHTML = '<i class="fa-solid fa-ban"></i>';
+        addDıv.appendChild(addButton2);
+    
+        todoList.appendChild(addDıv);
+    
+        todoInput.value = "";
+
+        success.style.display = 'block';
+        setTimeout(() => {
+            success.style.display = 'none';
+        }, 1500);
+    
     }
+    
 }
 
-function clearTodos(){
-    eklenecekler.splice(0, eklenecekler.length);
-    displayTodos();
+
+
+//silme fonksiyonu
+function checks(e) {
+    const item = e.target;
+    
+    if(item.classList[0] === 'deleteButton'){
+        const todo = item.parentElement;
+        todo.classList.add('fall');
+        removeLocaleStorage(todo)
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        } )
+    }
+
+    if (item.classList[0] === 'checkedButton') {
+        const checkLe = item.parentElement; //itemin içindeki elementleri kastediyoruz.
+        checkLe.classList.toggle('completed');//togle basıp çekme olayıdır.
+    }
+
 }
 
 
-displayTodos()
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (item) {
+        switch (e.target.value) {
+            case "all":
+                item.style.display = "flex";
+                break;
+            case "completed":
+                if (item.classList.contains("completed")) {
+                    item.style.display = "flex";
+                } else {
+                    item.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!item.classList.contains("completed")) {
+                    item.style.display = "flex";
+                } else {
+                    item.style.display = "none";
+                }
+                break;
+        }
+    })
+}
+
+//! locale Storage
+function saveLocalTodos(todo) {
+    let todos;
+    if (localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+function getTodos(){
+    let todos;
+    if (localStorage.getItem("todos") === null){
+        todos = [];
+    }else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach((todo) =>{
+        const addDıv = document.createElement("div");
+        addDıv.classList.add('todo');
+        
+        const addButton1 = document.createElement('button');
+        addButton1.classList.add('checkedButton');
+        addButton1.innerHTML = '<i class="fa-solid fa-check"></i>';
+        addDıv.appendChild(addButton1);
+    
+        const todoItem = document.createElement('li');
+        todoItem.classList.add('todo-item');
+        todoItem.innerText = todo;
+        addDıv.appendChild(todoItem);
+    
+        const addButton2 = document.createElement('button');
+        addButton2.classList.add('deleteButton');
+        addButton2.innerHTML = '<i class="fa-solid fa-ban"></i>';
+        addDıv.appendChild(addButton2);
+    
+        todoList.appendChild(addDıv);
+    });
+
+}
 
 
+    function removeLocaleStorage(todo) {
+        let todos;
+        if (localStorage.getItem("todos") === null) {
+            todos = [];
+        } else {
+            todos = JSON.parse(localStorage.getItem("todos"));
+        }
+        const todoIndex = todo.children[1].innerText;
+        todos.splice(todos.indexOf(todoIndex), 1);
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+    
+
+  
